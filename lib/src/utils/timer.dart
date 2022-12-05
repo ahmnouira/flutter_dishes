fd064e29478c3d425c5343a0beac095ff78538f4
Ahmed Nouira <ahmnouira@gmail.com>
2022-12-05 14:35:30 +0100

    feat: added timer utils

---
+++ lib/src/utils/timer.dart
@@ -0,0 +1,15 @@
+import 'dart:async' show FutureOr;
+
+void delaySeconds({
+  int seconds = 2,
+  FutureOr<dynamic> Function()? callback,
+}) {
+  Future.delayed(Duration(seconds: seconds), callback);
+}
+
+void delayMilliseconds({
+  int milliseconds = 700,
+  FutureOr<dynamic> Function()? callback,
+}) {
+  Future.delayed(Duration(milliseconds: milliseconds), callback);
+}
