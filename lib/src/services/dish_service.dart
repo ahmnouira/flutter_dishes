6c6d80c496b2e4e627d8419e087756f73e5566dc
Ahmed Nouira <ahmnouira@gmail.com>
2022-12-05 20:14:44 +0100

    feat: added dish service

---
+++ lib/src/services/dish_service.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dishes/src/data/model/dish_model.dart';
+
+class DishService {
+  final CollectionReference _collectionReference =
+      FirebaseFirestore.instance.collection('dishes');
+
+  /// Saving [Dish]
+  void add(Dish dish) {
+    _collectionReference.add(dish.toJSON());
+  }
+
+  /// Deleting [Dish]
+  Future<void> delete(String id) async {
+    return _collectionReference.doc(id).delete();
+  }
+
+  /// Editing [Dish]
+  Future<void> edit(String id, Dish dish) async {
+    return _collectionReference.doc(id).update(dish.toJSON());
+  }
+
+  /// Retrieve stream of data
+  Stream<QuerySnapshot> getAll() {
+    return _collectionReference.snapshots();
+  }
+}
